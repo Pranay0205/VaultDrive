2b1dd68d3330712d1e326492f8b567b11f06e2cf
Arundhati Das <arundhutidas2000@gmail.com>
2025-12-06 10:19:07 -0500

    fix: Update VaultIcon component to use imported SVG asset

--- vaultdrive_client/src/components/ui/vault-icon.tsx
+++ vaultdrive_client/src/components/ui/vault-icon.tsx
@@ -1,7 +1,9 @@
+import vaultSvg from "../../assets/vault.svg";
+
 interface VaultIconProps {
   className?: string;
 }
 
 export default function VaultIcon({ className = "w-8 h-8" }: VaultIconProps) {
-  return <img src="/src/assets/vault.svg" alt="VaultDrive" className={className} />;
+  return <img src={vaultSvg} alt="VaultDrive" className={className} />;
 }
